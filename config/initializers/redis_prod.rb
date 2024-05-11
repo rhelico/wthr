77b699499cfc42aadf02d65e08abb319a0bc1a95
Cort Fritz <cort@rheli.co>
2024-05-11 15:37:03 -0700

    set production cache store to find redis

--- config/initializers/redis_prod.rb
+++ config/initializers/redis_prod.rb
@@ -1,3 +1,10 @@
 return unless Rails.env.production?
 require 'redis'
-$redis = Redis.new(url: ENV['REDIS_URL'])
+# $redis = Redis.new(url: ENV['REDIS_URL'])
+
+Rails.application.config.cache_store = :redis_cache_store, {
+  url: ENV['REDIS_URL'],
+  read_timeout: 20.0, # 20 seconds
+  connect_timeout: 10.0, # 10 seconds
+  reconnect_attempts: 3
+}
