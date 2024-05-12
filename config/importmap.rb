e812e9e4eb6d62a3c8503f657e311781eb6ec22e
Cort Fritz <cort@rheli.co>
2024-05-12 11:31:16 -0700

    try removing protocol to support https in heroku

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,4 @@
 # Pin npm packages by running ./bin/importmap
-
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
@@ -7,6 +6,8 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 # pin "@hotwired/stimulus-importmap-autoloader", to: "stimulus-importmap-autoloader.js"
 
 # Application-specific
-pin "stimulus-autocomplete", to: "https://ga.jspm.io/npm:stimulus-autocomplete@3.0.2/src/autocomplete.js"
-pin "lodash/debounce", to: "https://cdn.skypack.dev/lodash.debounce"
+pin "stimulus-autocomplete", to: "//ga.jspm.io/npm:stimulus-autocomplete@3.0.2/src/autocomplete.js"
+pin "lodash/debounce", to: "//cdn.skypack.dev/lodash.debounce"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+pin "application", to: "application.js"
